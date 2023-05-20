9c06d89e593eed09b1b63fa2ef402cf38026f2d1
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2023-05-20 15:26:41 -0300

    domain-driven-design

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ const address = new Address('Street X', '1', '00000-00', 'Montreal')
 const customer = new Customer('1', 'Emmanuel Vinícius', address)
 
 const items = [
-    new OrderItem('1', 'Pumpkin', 0.8),
-    new OrderItem('2', 'Grapes', 1.2),
-    new OrderItem('3', 'Apple', 0.5),
+    new OrderItem('1', 'Pumpkin', 0.8, '1'),
+    new OrderItem('2', 'Grapes', 1.2, '2'),
+    new OrderItem('3', 'Apple', 0.5, '3'),
 ]
 
 const order = new Order('1', customer.id, items)
